c927b6b6cbf8cabb52424cc5626382640a2dc0fb
carloscrusca <42319133+carloscrusca@users.noreply.github.com>
2021-05-10 23:05:49 -0300

    Initial commit

---
+++ Models/ApplicationProfileUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationPasswordProtected.Models
+{
+    public class ApplicationProfileUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        public byte[] ProfilePicture { get; set; }
+    }
+}
